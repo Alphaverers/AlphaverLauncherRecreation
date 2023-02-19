80d91ccc3247246c22de7e0668c796816a11c477
orange man <karos2646@gmail.com>
2023-02-19 21:58:22 +0330

    Revert "idk"
    This reverts commit b0ed932bd5ebc95be9148e69e93a42f856c39b5d, reversing
    changes made to a84f9175bf9e9d5e1acb02e5e61c718a96c182cf.

--- forms/Launcher.cs
+++ forms/Launcher.cs
@@ -61,15 +61,9 @@ namespace AlphaverLauncherRecreation
                 return false;
             }
         }
-<<<<<<< HEAD
         private async void playButton_ClickAsync(object sender, EventArgs e)
         {
 
-=======
-            private async void playButton_ClickAsync(object sender, EventArgs e)
-            {
-            
->>>>>>> effe288ee095235891ed3401136aad20e98837d0
             string version = settings.version;
 
             bool isItVanilla = settings.mod == "vanilla";
